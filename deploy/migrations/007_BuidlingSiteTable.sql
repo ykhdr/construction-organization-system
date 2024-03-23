d86ebfab0ca0dd677815ce81b17f304d109e39e5
ykhdr <yanhudorozhkov@gmail.com>
2024-03-23 17:33:57 +0700

    add unique constraint on address

--- deploy/migrations/007_BuidlingSiteTable.sql
+++ deploy/migrations/007_BuidlingSiteTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS building_site
 (
     id            serial PRIMARY KEY,
-    address       varchar(150) NOT NULL,
+    address       varchar(150) NOT NULL UNIQUE,
     management_id integer      NOT NULL REFERENCES construction_management (id),
     manager_id    integer      NOT NULL REFERENCES engineer_worker (id)
 )
