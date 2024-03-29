ad05ecb5d7e59097d580aba5d55495fe74be17d7
ykhdr <yanhudorozhkov@gmail.com>
2024-03-30 01:39:03 +0700

    rename table

--- deploy/migrations/033_ConstructionMachineryUsageTable.sql
+++ deploy/migrations/033_ConstructionMachineryUsageTable.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS work_schedule_construction_machinery
+CREATE TABLE IF NOT EXISTS construction_machinery_usage
 (
     work_schedule_id integer REFERENCES work_schedule (id),
     machinery_id     integer REFERENCES construction_machinery (id),
