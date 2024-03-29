1873748598c248503b0979b4ebc7f70e6e9299ec
ykhdr <yanhudorozhkov@gmail.com>
2024-03-30 01:39:17 +0700

    add UNIQUE constraint on project_id

--- deploy/migrations/018_ConstructionContractTable.sql
+++ deploy/migrations/018_ConstructionContractTable.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS construction_contract
     building_organization_id integer      NOT NULL REFERENCES building_organization (id),
     customer_organization_id integer      NOT NULL REFERENCES customer_organization (id),
     name                     varchar(150) NOT NULL,
-    project_id               integer      NOT NULL REFERENCES construction_project (id),
+    project_id               integer      NOT NULL UNIQUE REFERENCES construction_project (id),
     signing_date             date         NOT NULL CHECK ( signing_date >= to_date('01.01.1900', 'DD-MM-YYYY'))
 )
