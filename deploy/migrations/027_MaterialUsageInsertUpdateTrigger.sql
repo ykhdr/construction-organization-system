f593c0d0f1417a90e931ae42f6d1e3428ed71aec
ykhdr <yanhudorozhkov@gmail.com>
2024-03-30 18:05:32 +0700

    replace after on before

--- deploy/migrations/027_MaterialUsageInsertUpdateTrigger.sql
+++ deploy/migrations/027_MaterialUsageInsertUpdateTrigger.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE TRIGGER tr_estimate_AU_AI
-    AFTER INSERT OR UPDATE OF fact_quantity
+CREATE OR REPLACE TRIGGER tr_estimate_BU_BI
+    BEFORE INSERT OR UPDATE OF fact_quantity
     ON material_usage
     FOR EACH ROW
 EXECUTE PROCEDURE fn_estimate_update_last_update_date()
