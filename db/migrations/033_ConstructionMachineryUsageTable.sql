7730f4eb672264ba76708a57ea4b4980bc4ab2ea
ykhdr <yanhudorozhkov@gmail.com>
2024-03-31 19:32:32 +0700

    add primary key on fields

--- db/migrations/033_ConstructionMachineryUsageTable.sql
+++ db/migrations/033_ConstructionMachineryUsageTable.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS construction_machinery_usage
 (
     work_schedule_id integer REFERENCES work_schedule (id),
     machinery_id     integer REFERENCES construction_machinery (id),
-    quantity         integer NOT NULL
+    quantity         integer NOT NULL,
+    PRIMARY KEY (work_schedule_id, machinery_id)
 )
