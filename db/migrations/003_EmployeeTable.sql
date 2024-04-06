566a07414e39bdc5a7446feed1c6127ef0fc6128
ykhdr <yanhudorozhkov@gmail.com>
2024-04-06 11:37:41 +0700

    add nn constraint on patronymic

--- db/migrations/003_EmployeeTable.sql
+++ db/migrations/003_EmployeeTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS employee
     id                       serial PRIMARY KEY,
     name                     varchar(100) NOT NULL,
     surname                  varchar(100) NOT NULL,
-    patronymic               varchar(100),
+    patronymic               varchar(100) NOT NULL,
     age                      integer      NOT NULL CHECK ( age > 0 ),
     seniority                integer      NOT NULL CHECK ( seniority > 0 ),
     building_organization_id integer      NOT NULL REFERENCES building_organization (id)
