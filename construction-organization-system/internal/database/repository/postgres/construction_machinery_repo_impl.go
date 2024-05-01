10f99941c6042aaaa9f5f33213d563ce9f25fa3f
ykhdr <yanhudorozhkov@gmail.com>
2024-05-02 02:23:35 +0700

    fix query

--- construction-organization-system/internal/database/repository/postgres/construction_machinery_repo_impl.go
+++ construction-organization-system/internal/database/repository/postgres/construction_machinery_repo_impl.go
@@ -125,7 +125,7 @@ func (repo *constructionMachineryRepository) GetByProjectWithPeriod(ctx context.
 	FROM construction_project AS cp 
 		JOIN work_schedule AS ws ON cp.id = ws.project_id
         JOIN construction_machinery_usage AS cmu ON ws.id = cmu.work_schedule_id
-        JOIN construction_machinery AS cm ON cp.id = cm.project_id
+        JOIN construction_machinery AS cm ON cm.id = cmu.machinery_id
 	WHERE cp.id = $1
 		AND cp.id != 0
 		AND ($2 = '' OR ws.fact_start_date >= to_date($2,'YYYY-MM-DD'))
