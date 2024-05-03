dac07dc62aea2147ce84985782d6d591355e9c72
ykhdr <yanhudorozhkov@gmail.com>
2024-05-03 09:14:42 +0700

    fix tables

--- db/migrations/004_EngineerWorkerTable.sql
+++ db/migrations/004_EngineerWorkerTable.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS engineer_worker
 (
-    position_id integer     NOT NULL REFERENCES engineer_position (id),
+    position_id integer NOT NULL REFERENCES engineer_position (id),
+    team_id     integer NOT NULL REFERENCES engineer_team (id),
     UNIQUE (id)
 ) INHERITS (employee)
--- db/migrations/005_ConstructionWorkerTable.sql
+++ db/migrations/005_ConstructionWorkerTable.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS construction_worker
 (
     is_shift_worker boolean NOT NULL,
+    team_id         integer NOT NULL REFERENCES construction_team (id),
     UNIQUE (id)
 ) INHERITS (employee)
