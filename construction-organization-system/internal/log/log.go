0def883a958d6c9392e52cbe2573a620df096bb8
ykhdr <yanhudorozhkov@gmail.com>
2024-05-10 13:16:15 +0700

    update log format

--- construction-organization-system/internal/log/log.go
+++ construction-organization-system/internal/log/log.go
@@ -6,7 +6,11 @@ import (
 )
 
 var Logger = &logrus.Logger{
-	Out:       os.Stdout,
-	Level:     logrus.DebugLevel,
-	Formatter: &logrus.TextFormatter{},
+	Out:   os.Stdout,
+	Level: logrus.DebugLevel,
+	Formatter: &logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		DisableColors:   false,
+	},
 }
