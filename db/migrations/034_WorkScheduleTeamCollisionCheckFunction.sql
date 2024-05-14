f25333e32bcd61c09b8064586a617e2dc694ab0c
ykhdr <yanhudorozhkov@gmail.com>
2024-05-14 14:56:41 +0700

    fix migration script

--- db/migrations/034_WorkScheduleTeamCollisionCheckFunction.sql
+++ db/migrations/034_WorkScheduleTeamCollisionCheckFunction.sql
@@ -5,8 +5,8 @@ BEGIN
     IF EXISTS(SELECT *
               FROM work_schedule
               WHERE construction_team_id = NEW.construction_team_id
-                AND (NEW.plan_start_date BETWEEN plan_start_date AND plan_end_date OR
-                     NEW.fact_start_date BETWEEN fact_start_date AND fact_end_date))
+                AND ((NEW.plan_start_date BETWEEN plan_start_date AND plan_end_date) OR
+                     (NEW.fact_start_date BETWEEN fact_start_date AND fact_end_date)))
     THEN
         RAISE WARNING 'Team has a work in schedule at same time';
     END IF;
